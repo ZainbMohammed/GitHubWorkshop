168b9f0d57f5239e0c132fcedebd47b74e83d83b
Zainab <pk.x3311@gmail.com>
2024-07-13 04:43:08 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// main.js
+document.addEventListener("DOMContentLoaded", function() {
+    let display = document.querySelector('.display');
+    let buttons = Array.from(document.querySelectorAll('.button'));
+
+    buttons.map(button => {
+        button.addEventListener('click', (e) => {
+            if (e.target.innerText === 'C') {
+                display.innerText = '0';
+            } else if (e.target.innerText === '=') {
+                try {
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = 'Error';
+                }
+            } else {
+                if (display.innerText === '0') {
+                    display.innerText = e.target.innerText;
+                } else {
+                    display.innerText += e.target.innerText;
+                }
+            }
+        });
+    });
+});
